f7a75f0a43a929903f382f848cbcf1b30be06abe
kzhao27 <kzhao27@asu.edu>
2019-11-04 15:52:39 -0700

    1031

--- resnet_main.py
+++ resnet_main.py
@@ -483,20 +483,20 @@ class CifarModelTrainer(object):
     #print(server.target)
     #self._session = tf.Session(server.target, config=config)
 
-    # sv = tf.train.Supervisor(is_chief=(FLAGS.task_index == 0),
-    #                            logdir=str(FLAGS.checkpoint_dir),
-    #                            init_op=m.init,
-    #                            summary_op=None,
-    #                            saver=m.saver,
-    #                            global_step=m.global_step,
-    #                            save_model_secs=60)
-    # print(server.target)
-    # self._session = sv.prepare_or_wait_for_session(server.target, config=config)
+    sv = tf.train.Supervisor(is_chief=(FLAGS.task_index == 0),
+                               logdir=str(FLAGS.checkpoint_dir),
+                               init_op=m.init,
+                               summary_op=None,
+                               saver=m.saver,
+                               global_step=m.global_step,
+                               save_model_secs=60)
+    print(server.target)
+    self._session = sv.prepare_or_wait_for_session(server.target, config=config)
 
-    self._session = tf.train.MonitoredTrainingSession(master=server.target,
-                                      is_chief=(FLAGS.task_index == 0),
-                                      checkpoint_dir=FLAGS.checkpoint_dir
-                                                      )
+    # self._session = tf.train.MonitoredTrainingSession(master=server.target,
+    #                                   is_chief=(FLAGS.task_index == 0),
+    #                                   checkpoint_dir=FLAGS.checkpoint_dir)
+
 
     #self.session.run(m.init)
     #self.extract_model_spec()

af50a1ff57672cbba06cc1f258c44b29d55de087
kzhao27 <kzhao27@asu.edu>
2019-11-04 17:55:10 -0700

    1031

--- helper_utils.py
+++ helper_utils.py
@@ -141,6 +141,7 @@ def run_epoch_training(session, model, data_loader, curr_epoch):
   steps_per_epoch = int(model.hparams.train_size / model.hparams.batch_size)
   tf.logging.info('steps per epoch: {}'.format(steps_per_epoch))
   curr_step = session.run(model.global_step)
+  print(curr_step)
   assert curr_step % steps_per_epoch == 0
 
   # Get the current learning rate for the model based on the current epoch
